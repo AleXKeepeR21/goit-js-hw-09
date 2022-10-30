6b61ddf1ac5f7de9bdff21cb0c74ccead871e342
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2022-10-30 12:39:34 +0200

    add code for 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyChangeColor = document.querySelector('body');
 
+let interval = null;
+
+stopBtn.setAttribute('disabled', true);
+
+startBtn.addEventListener('click', clickStartBtn);
+stopBtn.addEventListener('click', clickStopBtn);
+
+function clickStartBtn() {
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', true);
+  interval = setInterval(() => {
+    bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function clickStopBtn() {
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+  clearInterval(interval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
